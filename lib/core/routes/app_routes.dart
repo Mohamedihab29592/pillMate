486a6fc24a1cde429d84751f5b6c390a1e52aa33
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-11-06 12:01:07 +0200

    Add payment screen route to appRoutes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:pill_mate/features/payment/presentation/screens/payment_screen.dart';
 
 import '../../features/initial/splash/splash.dart';
 
-
 class Routes {
   static const String initialRoute = '/';
-
+  static const String paymentScreen = '/paymentScreen';
 }
 
 Widget appRoutes(String route, dynamic arg) {
   switch (route) {
     case Routes.initialRoute:
-     return const SplashScreen();
-
-
+      return const PaymentScreen();
+    case Routes.paymentScreen:
+      return const PaymentScreen();
 
     default:
       return const Scaffold(
